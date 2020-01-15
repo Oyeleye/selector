23f164cbef16a525f00fb4544497fbee3cadf64d
Oyeleye <oyeleyeoyedayo@gmail.com>
2020-01-15 10:41:41 +0200

    first commit

---
+++ selector.js
@@ -0,0 +1,11 @@
+var first =document.getElementById("first");
+
+
+var special = document.getElementsByClassName("special") [0];
+
+var p = document.getElementsByTagName("p");
+
+var first = document.querySelector("#first")
+
+var special = document.querySelector(".special")
+
